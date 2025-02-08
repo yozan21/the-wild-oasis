2ea67bf8ebadb1101b30d60995aad243e02d4a38
yozan21 <yojankaphle80@gmail.com>
2025-02-08 23:32:55 +0545

    Clear initial state

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("rabeto3369@andinews.com");
-  const [password, setPassword] = useState("test1234566");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLoading } = useLogin();
 
